35108d000c34079f11ace7938e359096992772cc
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-03-18 20:28:07 +0100

    set publish component as '/publish' routing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {WelcomeComponent} from './welcome/welcome.component';
+import {PublishComponent} from './publish/publish.component';
 
 const routes: Routes = [
   {
     path: '',
     component: WelcomeComponent
+  },
+  {
+    path: 'publish',
+    component: PublishComponent
   }
 ];
 

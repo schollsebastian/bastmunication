b508060432cbcf49604da67a92d815358551cab3
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-03-18 22:54:48 +0100

    add firebaseConfig to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDJVVjVxtakGpRMbXNeZ5i0xRohkF9FYIU',
+    authDomain: 'multiple-projects-9f123.firebaseapp.com',
+    databaseURL: 'https://multiple-projects-9f123.firebaseio.com',
+    projectId: 'multiple-projects-9f123',
+    storageBucket: 'multiple-projects-9f123.appspot.com',
+    messagingSenderId: '91371687144',
+    appId: '1:91371687144:web:7767c64ce65f82e7f5cd5d'
+  }
 };
